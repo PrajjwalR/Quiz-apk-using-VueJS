bd23fa8fe84d4a3fb1a30c50ea1e4a02fb019404
Prajjwal <pc732001@gmail.com>
2023-08-07 00:06:10 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,165 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const questions = ref([
+  {
+    question: "What is Vue JS?",
+    answer: 0,
+    options: ["A front end framework", "A library", "A mall with a hall"],
+    selected: null,
+  },
+  {
+    question: "What is Vue X?",
+    answer: 2,
+    options: ["A hall", "A book", "A state management library"],
+    selected: null,
+  },
+  {
+    question: "What is Vue Router used for?",
+    answer: 1,
+    options: [
+      "A backend",
+      "A routing library for Vue JS",
+      "A server",
+      "A sauce",
+    ],
+    selected: null,
+  },
+]);
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
+const score = computed(() => {
+  let value = 0;
+  questions.value.map((q) => {
+    if (q.selected == q.answer) {
+      value++;
+    }
+  });
+  return value;
+});
+
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value];
+  question.index = currentQuestion.value;
+  return question;
+});
+
+const SetAnswer = (evt) => {
+  questions.value[currentQuestion.value].selected = evt.target.value;
+  evt.target.value = null;
+};
+
+const NextQuestion = () => {
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++;
+  } else {
+    quizCompleted.value = true;
+  }
+};
+</script>
+
+<template>
+  <main class="app">
+    <h1>The Quiz</h1>
+
+    <section class="quiz" v-if="!quizCompleted">
+      <div class="quiz-info">
+        <span class="question">{{ getCurrentQuestion.question }}</span>
+        <span class="score">Score {{ score }}/{{ questions.length }}</span>
+      </div>
+
+      <div class="options">
+        <label
+          v-for="(option, index) in getCurrentQuestion.options"
+          :key="index"
+          :class="`option ${
+            getCurrentQuestion.selected == index
+              ? index == getCurrentQuestion.answer
+                ? 'correct'
+                : 'wrong'
+              : ''
+          }
+          ${
+            getCurrentQuestion.selected != null &&
+            index != getCurrentQuestion.selected
+              ? 'disabled'
+              : ''
+          }
+          `"
+        >
+          <input
+            type="radio"
+            v-model="getCurrentQuestion.selected"
+            :disabled="getCurrentQuestion.selected"
+            @change="SetAnswer"
+            :name="getCurrentQuestion.index"
+            :value="index"
+          />
+          <span>{{ option }}</span>
+        </label>
+      </div>
+      <button @click="NextQuestion" :disabled="!getCurrentQuestion.selected">
+        {{
+          getCurrentQuestion.index == questions.length - 1
+            ? "Finish"
+            : getCurrentQuestion.selected == null
+            ? "Select an option"
+            : "Next Question"
+        }}
+      </button>
+    </section>
+    <section v-else>
+      <h2>You have finished the Quiz!</h2>
+      <p>Your score is {{ score }} / {{ questions.length }}</p>
+    </section>
+  </main>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+body {
+  background-color: #271c36;
+  color: #fff;
+}
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  min-height: 100vh;
+}
+h1 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+.quiz {
+  background-color: #383a4b;
+  padding: 1rem;
+  width: 100;
+  max-width: 640px;
+}
+.quiz-info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+.quiz-info .question {
+  color: #8f8f8f;
+  font-size: 1.25rem;
+}
+
+.quiz-info .score {
+  color: #fff;
+  font-size: 1.25rem;
+}
+.options {
+  display: block;
+  padding: 1rem;
+  background-color: #271c36;
+}
+</style>
